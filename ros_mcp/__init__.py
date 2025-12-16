4b584c30462872d4d6bbbbed2d2da53aed22ff60
Stefano <stefano.dallag@gmail.com>
2025-12-15 18:49:43 -0600

    Refactoring entrypoints for the repo

--- ros_mcp/__init__.py
+++ ros_mcp/__init__.py
@@ -3,7 +3,7 @@
 This package provides ROS MCP tools that can be registered with any FastMCP instance.
 """
 
-from ros_mcp.server import main, mcp
+from ros_mcp.main import main, mcp
 from ros_mcp.tools import register_ros_tools
 from ros_mcp.utils.websocket_manager import WebSocketManager
 
